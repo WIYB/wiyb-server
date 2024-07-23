b08f20b27326a224fa131901c53cf06d0f180e3a
nfl1ryxditimo12 <nfl1ryxditimo12@gmail.com>
2024-07-23 14:43:35 +0900

    add: QueryDSL 구성 추가

--- storage/build.gradle.kts
+++ storage/build.gradle.kts
@@ -1,3 +1,9 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
+plugins {
+    kotlin("kapt")
+}
+
 allOpen {
     annotation("jakarta.persistence.Entity")
     annotation("jakarta.persistence.MappedSuperclass")
@@ -8,7 +14,21 @@ dependencies {
     api("org.springframework.boot:spring-boot-starter-data-jpa")
 
     implementation("io.hypersistence:hypersistence-utils-hibernate-63:3.8.1")
+    implementation("com.querydsl:querydsl-jpa:5.1.0:jakarta")
+    implementation("com.querydsl:querydsl-apt:5.1.0:jakarta")
+    implementation("jakarta.persistence:jakarta.persistence-api")
+    implementation("jakarta.annotation:jakarta.annotation-api")
+
+    kapt("com.querydsl:querydsl-apt:5.1.0:jakarta")
 
     runtimeOnly("com.mysql:mysql-connector-j")
     runtimeOnly("com.h2database:h2")
+}
+
+tasks.named<BootJar>("bootJar") {
+    enabled = false
+}
+
+tasks.named<Jar>("jar") {
+    enabled = true
 }
