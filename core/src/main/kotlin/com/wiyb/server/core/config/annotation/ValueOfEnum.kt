027300e118440455a1da4cb482891d32e3ba4d95
nfl1ryxditimo12 <nfl1ryxditimo12@gmail.com>
2024-07-27 02:38:24 +0900

    add: Generic enum validator 추가

---
+++ core/src/main/kotlin/com/wiyb/server/core/config/annotation/ValueOfEnum.kt
@@ -0,0 +1,19 @@
+package com.wiyb.server.core.config.annotation
+
+import com.wiyb.server.core.domain.validation.EnumValidator
+import jakarta.validation.Constraint
+import jakarta.validation.Payload
+import kotlin.annotation.AnnotationRetention.RUNTIME
+import kotlin.annotation.AnnotationTarget.FIELD
+import kotlin.reflect.KClass
+
+@Target(FIELD)
+@Retention(RUNTIME)
+@MustBeDocumented
+@Constraint(validatedBy = [EnumValidator::class])
+annotation class ValueOfEnum(
+    val enumClass: KClass<out Enum<*>>,
+    val message: String = "",
+    val groups: Array<KClass<*>> = [],
+    val payload: Array<KClass<out Payload>> = []
+)
---
+++ core/src/main/kotlin/com/wiyb/server/core/domain/validation/EnumValidator.kt
@@ -0,0 +1,29 @@
+package com.wiyb.server.core.domain.validation
+
+import com.wiyb.server.core.config.annotation.ValueOfEnum
+import jakarta.validation.ConstraintValidator
+import jakarta.validation.ConstraintValidatorContext
+
+class EnumValidator : ConstraintValidator<ValueOfEnum, CharSequence> {
+    private val acceptedValues: MutableList<String> = mutableListOf()
+
+    override fun initialize(constraintAnnotation: ValueOfEnum) {
+        super.initialize(constraintAnnotation)
+
+        acceptedValues.addAll(
+            constraintAnnotation.enumClass.java
+                .enumConstants
+                .map { it.name }
+        )
+    }
+
+    override fun isValid(
+        value: CharSequence?,
+        context: ConstraintValidatorContext
+    ): Boolean =
+        if (value == null) {
+            true
+        } else {
+            acceptedValues.contains(value.toString())
+        }
+}
