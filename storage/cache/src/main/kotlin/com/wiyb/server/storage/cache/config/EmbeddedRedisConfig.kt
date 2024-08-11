74ca72624e32b2f3ab699594d1d03a093e59ce5d
nfl1ryxditimo12 <nfl1ryxditimo12@gmail.com>
2024-08-11 19:55:13 +0900

    feat: embedded redis 범위 축소

--- storage/cache/src/main/kotlin/com/wiyb/server/storage/cache/config/EmbeddedRedisConfig.kt
+++ storage/cache/src/main/kotlin/com/wiyb/server/storage/cache/config/EmbeddedRedisConfig.kt
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import redis.embedded.RedisServer
 
-@Profile("local", "local-dev", "test")
+@Profile("local", "test")
 @Configuration()
 class EmbeddedRedisConfig(
     @Value("\${spring.redis.port}")
