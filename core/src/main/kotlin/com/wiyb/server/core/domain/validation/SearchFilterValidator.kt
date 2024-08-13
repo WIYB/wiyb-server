4a4233b6627f7069e9feae79bfa3e67ee9792630
nfl1ryxditimo12 <nfl1ryxditimo12@gmail.com>
2024-08-14 04:14:39 +0900

    feat: 검색 쿼리 유효성 검사 추가

---
+++ core/src/main/kotlin/com/wiyb/server/core/domain/validation/SearchFilterValidator.kt
@@ -0,0 +1,30 @@
+package com.wiyb.server.core.domain.validation
+
+import com.wiyb.server.core.config.annotation.SearchFilterKeywords
+import com.wiyb.server.storage.database.entity.golf.constant.BrandName
+import com.wiyb.server.storage.database.entity.golf.constant.EquipmentType
+import jakarta.validation.ConstraintValidator
+import jakarta.validation.ConstraintValidatorContext
+
+class SearchFilterValidator : ConstraintValidator<SearchFilterKeywords, CharSequence> {
+    private val acceptedValues: MutableList<String> = mutableListOf()
+
+    override fun initialize(constraintAnnotation: SearchFilterKeywords) {
+        super.initialize(constraintAnnotation)
+
+        acceptedValues.addAll(
+            BrandName.entries.map { it.getCode().uppercase() } +
+                EquipmentType.entries.map { it.getCode().uppercase() }
+        )
+    }
+
+    override fun isValid(
+        value: CharSequence?,
+        context: ConstraintValidatorContext
+    ): Boolean =
+        if (value == null) {
+            true
+        } else {
+            acceptedValues.containsAll(value.split(',').map { it.uppercase() })
+        }
+}
