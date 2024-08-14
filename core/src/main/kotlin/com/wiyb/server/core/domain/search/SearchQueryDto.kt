051ab73af7326c16c726064c3b951b51d0300b77
nfl1ryxditimo12 <nfl1ryxditimo12@gmail.com>
2024-08-14 17:40:59 +0900

    hotfix: 제약 조건 느슨하게

--- core/src/main/kotlin/com/wiyb/server/core/domain/search/SearchQueryDto.kt
+++ core/src/main/kotlin/com/wiyb/server/core/domain/search/SearchQueryDto.kt
@@ -3,12 +3,12 @@ package com.wiyb.server.core.domain.search
 import com.wiyb.server.core.config.annotation.SearchFilterKeywords
 import com.wiyb.server.core.config.annotation.ValueOfEnum
 import com.wiyb.server.storage.database.entity.golf.constant.SearchSortedBy
-import jakarta.validation.constraints.Min
-import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.NotBlank
+import org.hibernate.validator.constraints.Length
 
 data class SearchQueryDto(
-    @field:Min(2)
-    @field:NotNull
+    @field:Length(min = 2, max = 50, message = "keyword length must be between 2 and 50")
+    @field:NotBlank
     val keyword: String,
     @field:SearchFilterKeywords
     val filters: String?,
